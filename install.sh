b5aec8019b0a151de5b04e6600d5fd8a795db5d7
David Denton <denton.david@gmail.com>
2018-07-25 22:14:45 +0100

    adding setup for native build

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# get graalvm
+open http://download.oracle.com/otn/utilities_drivers/oracle-labs/graalvm-ee-1.0.0-rc4-macos-amd64.tar.gz
+
+# install zlib-dev
+xcode-select --install
